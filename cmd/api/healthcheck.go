54dde2ddc33bb6b1715553559bda2d3e1e76281b
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-11 10:39:50 -0800

    healthcheck variables working

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,5 +8,5 @@ import (
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %\n", version)
+	fmt.Fprintf(w, "version: %s\n", version)
 }
