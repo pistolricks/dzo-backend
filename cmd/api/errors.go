33d7d91d8a372a42f493a6842b0e34fbf5a0f412
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-14 03:26:29 -0800

    validation on createMovieHandler

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -41,3 +41,7 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 	message := fmt.Sprintf("the method %s is not allowed for the requested resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/ollivr/greenlight/internal/data"
+	"github.com/ollivr/greenlight/internal/validator"
 	"net/http"
 	"time"
 )
@@ -21,6 +22,26 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	v := validator.New()
+	v.Check(input.Title != "", "title", "is required")
+
+	v.Check(input.Year >= 1888, "year", "must be greater than or equal to 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(input.Runtime != 0, "runtime", "is required")
+	v.Check(input.Runtime > 0, "runtime", "must be a positive number")
+
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >= 1, "genres", "must contain at least one")
+	v.Check(len(input.Genres) <= 5, "genres", "must contain no more than five")
+	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
