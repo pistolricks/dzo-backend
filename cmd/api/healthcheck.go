0670b13145e283024b18014271d52301b93fded7
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-14 00:19:59 -0800

    added struct tag for snakecase

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not complete your request.", http.StatusInternalServerError)
+	}
 }
