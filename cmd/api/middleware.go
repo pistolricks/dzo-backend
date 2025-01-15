2b8df38e4e190bd8dad8ce44348ed1a58b6189e2
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-15 03:50:52 -0800

    added ip rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -19,13 +21,33 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock()
+
 		next.ServeHTTP(w, r)
 	})
+
 }
